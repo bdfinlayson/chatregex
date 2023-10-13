6f9b43495a63857450ac5df77d87508db17eb25f
warhodes-dev <warhodes@gmail.com>
2023-10-13 15:27:33 -0400

    trying to parse honorifics, initialisms, and punctuated names

--- src/chatregex/pipeline/chapter_reader.py
+++ src/chatregex/pipeline/chapter_reader.py
@@ -4,7 +4,7 @@ import re
 class ChapterReader:
     @staticmethod
     def split_by_chapter(text: str) -> list[str]:
-        chapters = re.split(r'\n\n\nchapter', text, flags=re.IGNORECASE)[1:]
+        chapters = re.split(r'\n\n\nchapter.*\n.*\n', text, flags=re.IGNORECASE)[1:]
         return chapters
 
     @staticmethod
@@ -20,5 +20,12 @@ class ChapterReader:
 
     @staticmethod
     def split_by_sentence(text: str) -> list[str]:
+        # Preserve strings like "H. W. McGee" or "Dr. Watson"
+        # In other words, these periods are swapped with some token '@' 
+        # so that they aren't interpreted as sentence terminators
+        text = re.sub(r"(?:(?<=[A-Z])|(?<=Mrs)|(?<=Mr)|(?<=Ms)|(?<=Dr)|(?<=St))\.( )?", r'@\1', text)
         sentences = re.findall(r'[^.!?]+[.!?]', text)
+        for i in range(len(sentences)):
+            sentence = sentences[i]
+            #sentences[i] = re.sub(r"@", r'.', sentence)
         return sentences
